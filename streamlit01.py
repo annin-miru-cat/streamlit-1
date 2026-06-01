18b46ae894a081409beda123886c881c31eb5ae6
annin-miru-cat <anninmirucat@gmail.com>
2026-06-01 21:16:57 +0800

    Update streamlit01.py

--- streamlit01.py
+++ streamlit01.py
@@ -18,43 +18,7 @@ DeepGame is not just a storytelling tool but an interactive partner in the user'
             "temperature":1}
 while True:
             S=st.text_input("response")
-            response = client.models.generate_content( model='gemini-3.1-flash-lite-preview', config=configs, contents=S)
-            st.write(response.text)
-is_dark_mode = st.checkbox("深色模式")
+            if st.button("click"):
+                        response = client.models.generate_content( model='gemini-3.1-flash-lite-preview', config=configs, contents=S)
+                        st.write(response.text)
 
-if is_dark_mode:
-    bg_color = "#0E1117"
-    text_color = "#FFFFFF"
-    st.write(" 晚安！")
-else:
-    bg_color = "#FFFFFF"
-    text_color = "#000000"
-    st.write(" 早安！")
-
-st.markdown(f"""
-    <style>.stApp {{
-        background-color: {bg_color};
-        color: {text_color};
-    }}
-    </style>
-    """, unsafe_allow_html=True)
-st.write("Hello, Streamlit!")
-st.title("大標題")
-st.header("副標題")
-st.write("一般文字")
-x = 11
-y = 6
-st.write(x+y)
-st.write(x*y)
-st.write(x/y)
-st.write(x//y)
-st.write(x%y)
-user_name = st.text_input("請問你的大名？")
-if user_name:
-    st.write(f" 歡迎來到 Python 課程，{user_name} 同學！")
-birth_year = st.number_input(
-    "請輸入你的出生西元年",
-    1900, 2026, 1995
-)
-age = 2026 - birth_year
-st.write(f"你的年紀 {age}")
