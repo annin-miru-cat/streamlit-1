1cbc2d5ad7c76121685a74b3b6bf07f8e8e85b43
annin-miru-cat <anninmirucat@gmail.com>
2026-06-01 20:32:43 +0800

    Update streamlit01.py

--- streamlit01.py
+++ streamlit01.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import os from google
-import genai from dotenv
-import load_dotenv
+import os
+from google import genai
+from dotenv import load_dotenv
 load_dotenv()
 client = genai.Client(api_key=os.getenv("API"))
 response = client.models.generate_content( model='gemini-2.5-flash', contents='你好！')
