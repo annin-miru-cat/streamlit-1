58db15d11818c5bd895e786d39d0d387995b132f
annin-miru-cat <anninmirucat@gmail.com>
2026-06-08 20:21:03 +0800

    Update streamlit01.py

--- streamlit01.py
+++ streamlit01.py
@@ -16,5 +16,5 @@ model = genai.GenerativeModel(
 
 if s := st.chat_input("請輸入你的指令"):
             with st.chat_message("assistant"):
-                        re = model.generate_content(prompt)
+                        re = model.generate_content(prompts)
                         st.markdown(response.text)
